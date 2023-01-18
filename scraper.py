15699c64cbf4ca0bca2aeb94334295d495e83add
Kalle Heinonen <kalle.j.heinonen@tuni.fi>
2023-01-19 00:08:18 +0200

    Version 0.0.3, Added extra data on each of the jobs found

--- scraper.py
+++ scraper.py
@@ -1,10 +1,15 @@
-"""
-This simple scraper was made by Kalle Heinonen, it was made for a specific topic, so it might need a few tweaks to work
-After making this I realised Duunitori does not allow robots, which could have been checked by using https://duunitori.fi/robots.txt.
-"""
+#This simple scraper was made by Kalle Heinonen, it was made for a specific topic, so it might need a few tweaks to work
+#After making this I realised Duunitori does not allow robots, which could have been checked by using https://duunitori.fi/robots.txt.
+
 import requests
 from bs4 import BeautifulSoup
 
+
+"""
+Potential improvements:
+Add the information to a csv file, remember to use "a" option not to overwrite previous data so you can save the data every time
+a job posting is found and not only when the program is finished.
+"""
 
 def main():
     #start a session
@@ -17,7 +22,7 @@ def main():
         #the website design made it so 1 redirects to a completely different search results
         if the_page != 1:
             #Get an url and delete the number on it
-            URL = "https://duunitori.fi/tyopaikat?alue=Helsinki%3BTampere&sivu=" + str(the_page)
+            URL = "https://duunitori.fi/tyopaikat?alue=Tampere%3BHelsinki&haku=Trainee&order_by=date_posted&sivu=" + str(the_page)
             page = s.get(URL)
             soup = BeautifulSoup(page.content, "html.parser")
             #Makes a list of all the jobs on the page
@@ -35,7 +40,21 @@ def main():
                 #more keywords can be added with an or statement or removed by removing the or statement and the text after that
                 if new_page.text.find("C++") != -1 or new_page.text.find("Python") != -1:
                     print(searchpage)
-            #goes to the next page
-            the_page += 1
+                    temp_soup = BeautifulSoup(new_page.content, "html.parser")
+                    #gets information like what company and where the job is
+                    job_post_data = temp_soup.find_all('p', class_="header__info")
+                    list_of_data = []
+                    for k in job_post_data:
+                        #The object is NavigableString so lets make it a normal string
+                        test = k.get_text()
+                        list_of_data.append(test)
+                    final_list = []
+                    for c in list_of_data:
+                        #and now lets remove \n:s from the data to make it readable
+                        final_list.append(c.replace("\n", ""))
+                    print("Company and location " + final_list[0])
+                    print("When was the application released and when is the last time to apply " + final_list[1])
+        #goes to the next page
+        the_page += 1
         
 main()
